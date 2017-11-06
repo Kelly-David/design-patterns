34511628900d740ef4f764144249fa380200d2cb
kelly-david <davidkelly.tlf@gmail.com>
2017-11-06 16:33:28 +0000

    Updated main

--- DecoratorPattern/DecoratorPattern/Program.cs
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -9,7 +9,7 @@ namespace DecoratorPattern
             Beverage beverage = new DarkRoast();
             beverage = new Mocha(beverage);
             beverage = new Soy(beverage);
-            beverage = new Whip(beverage);
+            beverage = new Grande(beverage);
             Console.WriteLine(beverage.GetDescription());
             Console.WriteLine("Total: $" + beverage.Cost());
             Console.ReadKey();
